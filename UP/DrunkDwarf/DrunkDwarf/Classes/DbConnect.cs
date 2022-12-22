1aae5913ccb66649438d37ab51d1db16e08d78e7
= <parframe@gmail.com>
2022-12-23 02:17:46 +0300

    UP

---
+++ UP/DrunkDwarf/DrunkDwarf/Classes/DbConnect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DrunkDwarf.Classes
+{
+    class DbConnect
+    {
+        public static Models.DrunkDwarfEntetiesEntities modeldb;
+    }
+}
