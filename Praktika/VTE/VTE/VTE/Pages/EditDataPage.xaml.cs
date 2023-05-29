bf8fe5040f0c28878dd62ed5eb68b09aa0d37124
Pavel Panyushkin <40539112+Chezemy@users.noreply.github.com>
2023-05-30 00:42:38 +0300

    upd

---
+++ Praktika/VTE/VTE/VTE/Pages/EditDataPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using VTE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VTE.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для EditDataPage.xaml
+    /// </summary>
+    public partial class EditDataPage : Page
+    {
+        public EditDataPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                uslugi usl = new uslugi();
+                int a = Int32.Parse(Price.Text);
+                int b = Int32.Parse(IdTbx.Text);
+                usl.ID =b;
+                usl.price = a;
+                usl.title = CmbTitle.Text;
+            
+                AppData.db.uslugi.Add(usl);
+                AppData.db.SaveChanges();
+                MessageBox.Show("Данные внесены успешно");
+                NavigationService.GoBack();
+            }
+            
+            catch (Exception ex)
+            {
+                MessageBox.Show("Не удалось добавить данные", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+    }
+}
